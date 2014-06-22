1c2eddcbd9bee71118acdb4c40e81d3e31c41287
Mai <prakunchan@myvuw.ac.nz>
2014-06-22 15:42:27 +1200

    debug schema.js

--- schema.js
+++ schema.js
@@ -11,12 +11,42 @@ client.connect();
 //rc.then(function (tname,rc){
 	
 //});
-checkTable('users');
-function checkTable(tablename){
+checkTable('users',function(rc){
+	if(rc!=null && rc == 0){
+		client.connect();
+		var query = client.query("CREATE TABLE users("+
+							"user_id serial PRIMARY KEY,"+
+							"name text NOT NULL, "+
+							"email text NOT NULL, "+
+							"password text NOT NULL, "+
+							"bio text "+
+							");"+
+							"INSERT INTO users (name, email, password)"+
+							" VALUES ('admin','admin', '$1');", [pw]);
+
+		query.on('error',function(err){
+			console.log(err);
+			client.end();
+		});
+		query.on('end',function(result){
+			console.log('after create table '+JSON.stringify(result));
+			client.end();
+		});
+	}
+	else{
+	//table exists
+	}
+		
+
+});
+
+function checkTable(tablename,callback){
 	//var deferred = new $.Deferred();
 	var q = client.query("select tablename from pg_catalog.pg_tables where tablename = $1",[tablename]);
 	q.on('error',function (err){
 		console.log(err);
+		client.end();
+		callback(null);
 	});
 	q.on('row',function(row,result){
 		console.log(JSON.stringify(row));
@@ -25,33 +55,12 @@ function checkTable(tablename){
 		client.end();
 		console.log(JSON.stringify(result));
 		if(result.rowCount==0){
-		console.log('no such table exists');
-		var pw = bcrypt.hashSync('admin', bcrypt.genSaltSync(8),null);
-		console.log(pw);
-		console.log(JSON.stringify(result.fields));
-		if(result.fields.name == 'users'){
-		client.connect();
-			var query = client.query("CREATE TABLE users("+
-								"user_id serial PRIMARY KEY,"+
-								"name text NOT NULL, "+
-								"email text NOT NULL, "+
-								"password text NOT NULL, "+
-								"bio text "+
-								");"+
-								"INSERT INTO users (name, email, password)"+
-								" VALUES ('admin','admin', '$1');", [pw]);
-			query.on('error',function(err){
-				console.log(err);
-			});
-			query.on('end',function(result){
-				console.log('after create table '+JSON.stringify(result));
-				client.end();
-			});
+			console.log('no such table exists');
+			callback(0);
 		}
-		
-	}
-	//	deferred.resolve();
-	client.end();
-	})(tablename));
-	//return deferred.promise();
+		else{
+			callback(result.rowCount);
+		}
+	});	
+
 }
