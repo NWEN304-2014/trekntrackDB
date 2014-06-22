a5f1fb513786d64ef42354b909beda022b1e99d3
Mai <prakunchan@myvuw.ac.nz>
2014-06-22 22:49:15 +1200

    passport signup

--- app/config/passport.js
+++ app/config/passport.js
@@ -49,7 +49,7 @@ module.exports = function(passport) {
 
 		// find a user whose email is the same as the forms email
 		// we are checking to see if the user trying to login already exists
-        User.findOne('email',email, function(err, user) {
+        User.findone('email',email, function(err, user) {
             // if there are any errors, return the error
             if (err == true){
                 return done(err);
