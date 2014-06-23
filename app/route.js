5a41a18a66c6d0db81954f08708be7eb983a4998
Mai <prakunchan@myvuw.ac.nz>
2014-06-23 21:14:56 +1200

    test post

--- app/route.js
+++ app/route.js
@@ -41,7 +41,7 @@ app.all('*',function(req,res,next){
 				}    
 				req.logIn(user, function(err) {  
 				if (err) { return next(err); }      
-					return res.jsonp('index');    
+					return res.jsonp(user+'redirectProfile');    
 				});
 			})(req, res, next);
 			// res.send("post message return");
